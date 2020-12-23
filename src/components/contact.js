bd4cfcadf9ee1411a48676d1dc45046704bfc7a8
Alex <asimak4@gmail.com>
2020-12-22 20:37:29 -0500

    fixing contact page issue

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,13 +4,13 @@ class Contact extends Component {
   render() {
     return(
       <div className="contact-body">
-        <Grid className="contact-grid">
+        <Grid style={{display:'flex', justifyContent:'center'}} className="contact-grid">
           <Cell col={6}>
             <h2>Alex Simak</h2>
             <img
               src={require('./alexgrad.jpg')}
               alt=""
-              style={{height:'500px',padding:'5%'}}
+              style={{height:'400px',paddingBottom:'0%'}}
                />
              
 
@@ -22,16 +22,16 @@ class Contact extends Component {
             <div className="contact-list">
               <List>
                 <ListItem>
-                  <ListItemContent style={{fontSize: '150%', fontFamily: 'Anton',alignSelf:"auto",paddingLeft:'23%'}}>
+                  <ListItemContent style={{display:'flex', justifyContent:'center',fontSize: '100%', fontFamily: 'Anton',alignSelf:"auto"}}>
                     <i className="fa fa-phone-square" aria-hidden="true"/>
                     (443)-474-0722
                   </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{fontSize: '150%', fontFamily: 'Anton',paddingLeft:'23%'}}>
+                  <ListItemContent style={{display:'flex', justifyContent:'center',fontSize: '100%', fontFamily: 'Anton'}}>
                     <i className="fa fa-envelope" aria-hidden="true"/>
-                          asimak4@gmail.com
+                      asimak4@gmail.com
                   </ListItemContent>
                 </ListItem>
 
